857912c80123bc44f15e82439d42d3c286363dd1
PhiBlue <ruthschoenberg@gmail.com>
2019-03-20 18:51:15 -0700

    Pushing final game

---
+++ Final Assignmenta/Assets/Supercyan Character Pack Free Sample/Scripts/SimpleCharacterControl.cs
@@ -0,0 +1,234 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class SimpleCharacterControl : MonoBehaviour {
+
+    private enum ControlMode
+    {
+        Tank,
+        Direct
+    }
+
+    [SerializeField] private float m_moveSpeed = 2;
+    [SerializeField] private float m_turnSpeed = 200;
+    [SerializeField] private float m_jumpForce = 4;
+    [SerializeField] private Animator m_animator;
+    [SerializeField] private Rigidbody m_rigidBody;
+
+    [SerializeField] private ControlMode m_controlMode = ControlMode.Direct;
+
+    private float m_currentV = 0;
+    private float m_currentH = 0;
+
+    private readonly float m_interpolation = 10;
+    private readonly float runSpeed = 1.33f;
+    private readonly float m_backwardsWalkScale = 0.66f;
+    private readonly float m_backwardRunScale = 1;
+
+    private bool m_wasGrounded;
+    private Vector3 m_currentDirection = Vector3.zero;
+
+    private float m_jumpTimeStamp = 0;
+    private float m_minJumpInterval = 0.25f;
+
+    private bool m_isGrounded;
+    private List<Collider> m_collisions = new List<Collider>();
+    bool multijump = false;
+    int jumpcount;
+    bool run = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "multijump")
+        {
+            multijump = true;
+            jumpcount = 2;
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "run")
+        {
+            run = true;
+            Destroy(other.gameObject);
+        }
+        if (other.tag == "float")
+        {
+            m_jumpForce *= 2;
+            Destroy(other.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        for(int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                if (!m_collisions.Contains(collision.collider)) {
+                    m_collisions.Add(collision.collider);
+                }
+                m_isGrounded = true;
+            }
+        }
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        ContactPoint[] contactPoints = collision.contacts;
+        bool validSurfaceNormal = false;
+        for (int i = 0; i < contactPoints.Length; i++)
+        {
+            if (Vector3.Dot(contactPoints[i].normal, Vector3.up) > 0.5f)
+            {
+                validSurfaceNormal = true; break;
+            }
+        }
+
+        if(validSurfaceNormal)
+        {
+            m_isGrounded = true;
+            if (!m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Add(collision.collider);
+            }
+        } else
+        {
+            if (m_collisions.Contains(collision.collider))
+            {
+                m_collisions.Remove(collision.collider);
+            }
+            if (m_collisions.Count == 0) { m_isGrounded = false; }
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(m_collisions.Contains(collision.collider))
+        {
+            m_collisions.Remove(collision.collider);
+        }
+        if (m_collisions.Count == 0) { m_isGrounded = false; }
+    }
+
+	void Update () {
+        m_animator.SetBool("Grounded", m_isGrounded);
+
+        switch(m_controlMode)
+        {
+            case ControlMode.Direct:
+                DirectUpdate();
+                break;
+
+            case ControlMode.Tank:
+                TankUpdate();
+                break;
+
+            default:
+                Debug.LogError("Unsupported state");
+                break;
+        }
+
+        m_wasGrounded = m_isGrounded;
+    }
+
+    private void TankUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        bool walk = Input.GetKey(KeyCode.LeftShift);
+
+        if (v < 0) {
+            if (walk && run) { v *= m_backwardRunScale; }
+            else { v *= m_backwardsWalkScale; }
+        } else if(walk&&run)
+        {
+            v *= runSpeed;
+        }
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        transform.position += transform.forward * m_currentV * m_moveSpeed * Time.deltaTime;
+        transform.Rotate(0, m_currentH * m_turnSpeed * Time.deltaTime, 0);
+
+        m_animator.SetFloat("MoveSpeed", m_currentV);
+
+        JumpingAndLanding();
+    }
+
+    private void DirectUpdate()
+    {
+        float v = Input.GetAxis("Vertical");
+        float h = Input.GetAxis("Horizontal");
+        Debug.Log("directed update");
+   
+        Transform camera = Camera.main.transform;
+        if (run)
+        {
+            Debug.Log("run is true");
+        }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            Debug.Log("shifted");
+        }
+
+        if (run == true && Input.GetKey(KeyCode.LeftShift))
+        {
+            Debug.Log("reached2");
+            v *= runSpeed;
+            h *= runSpeed;
+        }
+
+        m_currentV = Mathf.Lerp(m_currentV, v, Time.deltaTime * m_interpolation);
+        m_currentH = Mathf.Lerp(m_currentH, h, Time.deltaTime * m_interpolation);
+
+        Vector3 direction = camera.forward * m_currentV + camera.right * m_currentH;
+
+        float directionLength = direction.magnitude;
+        direction.y = 0;
+        direction = direction.normalized * directionLength;
+
+        if(direction != Vector3.zero)
+        {
+            m_currentDirection = Vector3.Slerp(m_currentDirection, direction, Time.deltaTime * m_interpolation);
+
+            transform.rotation = Quaternion.LookRotation(m_currentDirection);
+            transform.position += m_currentDirection * m_moveSpeed * Time.deltaTime;
+
+            m_animator.SetFloat("MoveSpeed", direction.magnitude);
+        }
+
+        JumpingAndLanding();
+    }
+
+    private void JumpingAndLanding()
+    {
+        bool jumpCooldownOver = (Time.time - m_jumpTimeStamp) >= m_minJumpInterval;
+
+        if (jumpCooldownOver && m_isGrounded && Input.GetKey(KeyCode.Space) && multijump==false)
+        {
+            m_jumpTimeStamp = Time.time;
+            m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+        }
+       else if (jumpCooldownOver && Input.GetKeyDown(KeyCode.Space) && jumpcount>0 && multijump==true)
+        {
+            if (jumpcount == 1)
+            {
+                m_animator.SetTrigger("Land");
+            }
+            m_jumpTimeStamp = Time.time;
+            m_rigidBody.AddForce(Vector3.up * m_jumpForce, ForceMode.Impulse);
+            jumpcount--;
+        }
+
+        if (!m_wasGrounded && m_isGrounded)
+        {
+            m_animator.SetTrigger("Land");
+            jumpcount = 2;
+        }
+
+        if (!m_isGrounded && m_wasGrounded)
+        {
+            m_animator.SetTrigger("Jump");
+        }
+    }
+}
---
+++ Final Assignmenta/Assets/respawn.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class respawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (gameObject.tag == "location1")
+        {
+            other.gameObject.transform.position = new Vector3(-1, 2.5f, 1.5f);
+            other.gameObject.transform.rotation = new Quaternion(0,0,0,0);
+        }
+        else if (gameObject.tag == "location2")
+        {
+            other.gameObject.transform.position = new Vector3(0.5f,5,-11.25f);
+        }
+        else if (gameObject.tag == "location3")
+        {
+            other.gameObject.transform.position = new Vector3(-22.5f, 2, -12);
+        }
+        else if (gameObject.tag == "location4")
+        {
+            other.gameObject.transform.position = new Vector3(-29.5f,7,-30);
+        }
+        else
+            Debug.LogError("Error");
+    }
+}
